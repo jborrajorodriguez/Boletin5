adc687b26431710ccd89bd60add59f4ab7078922
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-06 09:16:03 +0100

    Clase metodos

---
+++ Boletin5_4/src/boletin5_4/Persoas.java
@@ -0,0 +1,25 @@
+package boletin5_4;
+/**
+ * @author jborrajorodriguez
+ */
+public class Persoas {
+    
+   private String nome;
+   private int peso;
+   
+   public Persoas(){
+       
+   }
+   public void setNome(String nome){
+       this.nome=nome;
+   }
+   public String getNome(){
+       return nome;
+   }   
+   public void setPeso(int peso){
+       this.peso=peso;
+   }
+   public int getPeso(){
+       return peso;
+   }
+}
