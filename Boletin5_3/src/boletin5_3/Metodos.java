623bd01d1eb35d8a9614c42897d876f211d3dba0
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-06 09:15:39 +0100

    Clase metodos

---
+++ Boletin5_3/src/boletin5_3/Metodos.java
@@ -0,0 +1,20 @@
+package boletin5_3;
+/**
+ * @author jborrajorodriguez
+ */
+public class Metodos {
+    private int num;
+    
+    public Metodos(){
+        
+    }
+    
+    public void setNum(int num){
+        this.num=num;
+    }
+    public int getNum(){
+        return num;
+    }
+    
+    
+}
