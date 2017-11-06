efe537bae984d150499e9fb85198fe8a356a109a
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-06 09:16:24 +0100

    Clase metodos

---
+++ Boletin5_5/src/boletin5_5/Numeros.java
@@ -0,0 +1,34 @@
+package boletin5_5;
+
+/**
+ * @author jborrajorodriguez
+ */
+public class Numeros {
+    private int num1,num2,num3;
+    
+    public Numeros(){
+        
+    }
+    
+    public void setNum1(int num1){
+        this.num1=num1;
+        
+    }
+    public void setNum2(int num2){
+        this.num2=num2;
+        
+    }
+    public void setNum3(int num3){
+        this.num3=num3;
+    }
+    public int getNum1(){
+        return num1;
+    }
+    public int getNum2(){
+        return num2;
+    }
+    public int getNum3(){
+        return num3;
+    }
+
+}
